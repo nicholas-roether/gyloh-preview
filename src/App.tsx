0e9d9b2caf72ea7f0cc2eff14a6a58a36bab20e6
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-11 18:45:59 +0200

    Render PageNavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import PageNavBar from "./structure/PageNavBar";
 
 class App extends React.Component<{}> {
 
 	render() {
-		return <div>WIP</div>
+		return (
+			<div id="app">
+				<PageNavBar />
+			</div>
+		);
 	}
 }
 
