d8ec7de52bfaf5260b43dd16fa183ddc3e19f3f8
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-13 08:47:09 +0200

    Render example page with content

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ export default class App extends React.Component<{}> {
 	render() {
 		return (
 			<div id="app">
-				<Page />
+				<Page>
+					<h1>Content</h1>
+					<p>Page conent goes here</p>
+				</Page>
 			</div>
 		);
 	}
