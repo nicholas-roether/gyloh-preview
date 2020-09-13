e8dad443b5d5f57d4ec1e740a49aa80700a6b433
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-13 06:42:32 +0200

    Create PageBar component

---
+++ src/common/PageBar.tsx
@@ -0,0 +1,53 @@
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import MenuIcon from "@material-ui/icons/Menu";
+import CalendarIcon from "@material-ui/icons/CalendarToday"
+import React from "react";
+import theme from "../theme";
+import styled from "styled-components";
+
+export default class PageBar extends React.Component<{onOpenMenu?: ((e?: React.MouseEvent) => void)}> {
+	render() {
+		const Heading = styled.span`
+			margin: ${theme.spacing(0, 2)};
+		`;
+
+		const TitleLong = styled(Typography)`
+			display: none;
+			${theme.breakpoints.up("sm")} {
+				display: inline;
+			}
+		`;
+
+		const TitleShort = styled(Typography)`
+			display: inline;
+			${theme.breakpoints.up("sm")} {
+				display: none;
+			}
+		`;
+
+		const Icons = styled.span`
+			position: relative;
+			float: right;
+		`;
+
+		const Grow = styled.span`flex-grow: 1;`;
+
+		return (
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton color="inherit" onClick={this.props.onOpenMenu}><MenuIcon /></IconButton>
+					<Heading>
+						<TitleLong variant="h5">Gymnasium Lohbrügge</TitleLong>
+						<TitleShort variant="h5">Gyloh</TitleShort>
+					</Heading>
+					<Grow />
+					<Icons>
+						<IconButton color="inherit" aria-label="Seite durchsuchen"><SearchIcon /></IconButton>
+						<IconButton color="inherit" aria-label="Kalender ansehen"><CalendarIcon /></IconButton>
+					</Icons>
+				</Toolbar>
+			</AppBar>
+		);
+	}
+}
