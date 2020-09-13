2267d176b69105d729a1ad94344c06cca2cfba05
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-13 08:46:55 +0200

    Add animations and content

--- src/common/Page.tsx
+++ src/common/Page.tsx
@@ -1,8 +1,31 @@
+import { Box, createStyles, WithStyles, withStyles } from "@material-ui/core";
 import React from "react";
+import theme from "../theme";
 import PageBar from "./PageBar";
 import PageNav from "./PageNav";
+import { DRAWER_WIDTH } from "./SideNav";
 
-export default class Page extends React.Component {
+const styles = createStyles({
+	slideOpen: {
+		width: `calc(100% - ${DRAWER_WIDTH}px)`,
+		marginLeft: theme.direction === "ltr" ? DRAWER_WIDTH : 0,
+		marginRight: theme.direction === "ltr" ? 0 : DRAWER_WIDTH,
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+    	}),
+	},
+	slideClose: {
+		width: "100%",
+		margin: 0,
+		transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen,
+    	}),
+	}
+});
+
+class Page extends React.Component<WithStyles<typeof styles>> {
 	state = {navOpen: false};
 
 	private openNav() {
@@ -14,11 +37,19 @@ export default class Page extends React.Component {
 	}
 
 	render() {
+		let { classes } = this.props; 
+		let slideClass = this.state.navOpen ? classes.slideOpen : classes.slideClose;
+
 		return (
 			<div className="page">
-				<PageBar onOpenMenu={() => this.openNav()} />
+				<PageBar onOpenMenu={() => this.openNav()} className={slideClass} />
 				<PageNav open={this.state.navOpen} onClose={() => this.closeNav()} />
+				<Box className={slideClass} pl={3} pr={3}>
+					{this.props.children}
+				</Box>
 			</div>
 		);
 	}
 }
+
+export default withStyles(styles)(Page);
