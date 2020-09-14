11a2d03224614ab85b10b4db03283d4733eb44f8
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-14 02:09:35 +0200

    Add classesIf and MouseEventListener

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,14 @@
+export type MouseEventListener = (e: React.MouseEvent) => void;
+
 export function getHTMLElems(elems: HTMLCollection) {
 	let children = new Array<HTMLElement>();
 	Array.from(elems).forEach(e => e instanceof HTMLElement && children.push(e));
 	return children;
+}
+
+export function classesIf(...classes: ([string, boolean] | string)[]): string {
+	return classes
+		.filter(t => typeof t === "string" || t[1])
+		.map<string>(t => typeof t === "string" ? t : t[0])
+		.join(" ");
 }
