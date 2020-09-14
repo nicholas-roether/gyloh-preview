7e8c7458bf75acf8480cabcb9368a5e309f20381
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-14 02:11:06 +0200

    Add searchbar

--- src/common/PageBar.tsx
+++ src/common/PageBar.tsx
@@ -1,12 +1,22 @@
 import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
 import MenuIcon from "@material-ui/icons/Menu";
 import CalendarIcon from "@material-ui/icons/CalendarToday"
 import React from "react";
 import theme from "../theme";
 import styled from "styled-components";
+import Search from "./Search";
 
 export default class PageBar extends React.Component<{onOpenMenu?: ((e?: React.MouseEvent) => void), className?: string}> {
+	state = {searchOpen: false}
+
+	private openSearch() {
+		this.setState({searchOpen: true});
+	}
+
+	private closeSearch() {
+		this.setState({searchOpen: false});
+	}
+
 	render() {
 		const Heading = styled.span`
 			margin: ${theme.spacing(0, 2)};
@@ -14,21 +24,30 @@ export default class PageBar extends React.Component<{onOpenMenu?: ((e?: React.M
 
 		const TitleLong = styled(Typography)`
 			display: none;
-			${theme.breakpoints.up("sm")} {
+			${theme.breakpoints.up("md")} {
 				display: inline;
 			}
 		`;
 
 		const TitleShort = styled(Typography)`
-			display: inline;
+			display: none;
 			${theme.breakpoints.up("sm")} {
+				display: inline;
+			}
+			${theme.breakpoints.up("md")} {
 				display: none;
 			}
 		`;
 
 		const Icons = styled.span`
-			position: relative;
-			float: right;
+			display: inline-flex;
+			flex-grow: 1;
+			justify-content: flex-end;
+			${theme.breakpoints.up("sm")} {
+				flex-grow: 0;
+				position: relative;
+				float: right;
+			}
 		`;
 
 		const Grow = styled.span`flex-grow: 1;`;
@@ -43,7 +62,7 @@ export default class PageBar extends React.Component<{onOpenMenu?: ((e?: React.M
 					</Heading>
 					<Grow />
 					<Icons>
-						<IconButton color="inherit" aria-label="Seite durchsuchen"><SearchIcon /></IconButton>
+						<Search open={this.state.searchOpen} onOpen={() => this.openSearch()} onClose={() => this.closeSearch()} />
 						<IconButton color="inherit" aria-label="Kalender ansehen"><CalendarIcon /></IconButton>
 					</Icons>
 				</Toolbar>
