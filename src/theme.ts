7268cbb5717c74cc74db4ac2440c3ff734730fca
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-16 23:42:20 +0200

    Change heading fonts to Rubik

--- src/theme.ts
+++ src/theme.ts
@@ -22,7 +22,25 @@ const theme = createMuiTheme({
 			fontSize: 15,
 			fontFamily: "Rubik, sans-serif",
 			textTransform: "uppercase",
-		}
+		},
+		h1: {
+			fontFamily: "Rubik, sans-serif"
+		},
+		h2: {
+			fontFamily: "Rubik, sans-serif"
+		},
+		h3: {
+			fontFamily: "Rubik, sans-serif"
+		},
+		h4: {
+			fontFamily: "Rubik, sans-serif"
+		},
+		h5: {
+			fontFamily: "Rubik, sans-serif"
+		},
+		h6: {
+			fontFamily: "Rubik, sans-serif"
+		},
 	},
 });
 
