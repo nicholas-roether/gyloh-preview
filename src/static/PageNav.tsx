f0ebaed0b9ae3b8f2a84f1291f713c04304b9659
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-16 17:22:42 +0200

    Render navigation.json

--- src/static/PageNav.tsx
+++ src/static/PageNav.tsx
@@ -1,22 +1,14 @@
 import { Box, Divider, ListItemIcon, ListItemText, Button } from "@material-ui/core";
-import { 
-	Home as HomeIcon, 
-	NewReleases as NewsIcon, 
-	Grade as GradesIcon,
-	Group as AboutIcon,
-	Info as InfoIcon,
-	Contacts as ContactIcon,
-	ContactMail as NewsletterIcon,
-	EmojiFoodBeverage as CafeteriaIcon,
-	Forum as AlumniIcon,
-	CalendarToday as PlannerIcon,
-} from "@material-ui/icons";
+import * as icons from "@material-ui/icons";
 import React from "react";
 import EduportLaunchpad from "./EduportLaunchpad";
 import ListItemCollapse from "../common/ListItemCollapse";
 import ListItemLink from "../common/ListItemLink";
 import SideNav, { SideNavHeader, SideNavList } from "../common/SideNav";
 import CollapseItem from "../common/CollapseItem";
+import { nav as navStructure } from "../structure/navigation.json";
+import { mapFrom } from "../util";
+
 
 export interface PageNavProps {
 	open: boolean;
@@ -24,9 +16,90 @@ export interface PageNavProps {
 	onClose: React.MouseEventHandler
 }
 
+interface CollapseItemData {
+	to: string,
+	text: string,
+}
+function isCollapseItemData(object: any): object is CollapseItemData {
+	return (
+		"to" in object &&
+		"text" in object
+	);
+}
+
+interface ListItemLinkData {
+	to: string,
+	text: string,
+	icon?: string
+}
+function isListItemLinkData(object: any): object is ListItemLinkData {
+	return isCollapseItemData(object);
+}
+
+interface ListItemCollapseData {
+	to?: string,
+	text: string,
+	icon?: string,
+	collapse: CollapseItemData[]
+}
+function isListItemCollapseData(object: any): object is ListItemCollapseData {
+	return (
+		"text" in object &&
+		"collapse" in object
+	)
+}
+
+type NavItem = ListItemLinkData | ListItemCollapseData;
+
 export default class PageNav extends React.Component<PageNavProps> {
+	private createIcon(name: string): React.ReactElement | null {
+		let iconMap = mapFrom(icons);
+		return React.createElement(iconMap.get(name));
+	}
+
+	private createListItemLink(data: ListItemLinkData): React.ReactElement {
+		return (
+			<ListItemLink to={data.to}>
+				{data.icon && <ListItemIcon>{this.createIcon(data.icon)}</ListItemIcon>}
+				<ListItemText primary={data.text} />
+			</ListItemLink>
+		);
+	}
+	
+	private createCollapseItem(data: CollapseItemData): React.ReactElement {
+		return (
+			<CollapseItem to={data.to}>
+				<ListItemText primary={data.text} />
+			</CollapseItem>
+		);
+	}
+
+	private createListItemCollapse(data: ListItemCollapseData): React.ReactElement {
+		return (
+			<ListItemCollapse 
+				to={data.to} 
+				text={<ListItemText>{data.text}</ListItemText>}
+				icon={data.icon ? <ListItemIcon>{this.createIcon(data.icon)}</ListItemIcon> : null}
+			>
+				{data.collapse.map(e => this.createCollapseItem(e))}
+			</ListItemCollapse>
+		);
+	}
+
+	private renderStructure(data: NavItem[]): React.ReactElement[] {
+		let elements: React.ReactElement[] = [];
+		for(let navItem of data) {
+			if(isListItemCollapseData(navItem)) {
+				elements.push(this.createListItemCollapse(navItem));
+			}
+			else if(isListItemLinkData(navItem)) {
+				elements.push(this.createListItemLink(navItem));
+			}
+		}
+		return elements;
+	}
+
 	render() {
-		
 		return (
 			<SideNav
 				open={this.props.open}
@@ -39,75 +112,13 @@ export default class PageNav extends React.Component<PageNavProps> {
 					</Button>
 				</SideNavHeader>
 				<SideNavList>
-					{/* Include greeting here */}
-					<ListItemLink to="/">
-						<ListItemIcon><HomeIcon /></ListItemIcon>
-						<ListItemText primary="Home" />
-					</ListItemLink>
-					{/* -> Schulprofil */}
-					<ListItemCollapse to="/about" text={
-						<ListItemText>Über Uns</ListItemText>
-					} icon={
-						<ListItemIcon><AboutIcon/></ListItemIcon>
-					}>
-						<CollapseItem to="/about/menschen">
-							<ListItemText primary="Menschen" />
-						</CollapseItem>
-						<CollapseItem to="/about/fördern">
-							<ListItemText primary="Fördern &amp; Fordern" />
-						</CollapseItem>
-						<CollapseItem to="/about/beratung">
-							<ListItemText primary="Beratung" />
-						</CollapseItem>
-						<CollapseItem to="/about/ausland">
-							<ListItemText primary="Ausland" />
-						</CollapseItem>
-						<CollapseItem to="/about/projekte">
-							<ListItemText primary="Projekte &amp; Events" />
-						</CollapseItem>
-						<CollapseItem to="/about/partner">
-							<ListItemText primary="Partner" />
-						</CollapseItem>
-					</ListItemCollapse>
-					{/* use tabs */}
-					<ListItemLink to="/klassenstufen">
-						<ListItemIcon><GradesIcon /></ListItemIcon>
-						<ListItemText primary="Klassenstufen" />
-					</ListItemLink>
-					<ListItemLink to="/news">
-						<ListItemIcon><NewsIcon /></ListItemIcon>
-						<ListItemText primary="News" />
-					</ListItemLink>
-					<ListItemLink to="/info">
-						<ListItemIcon><InfoIcon /></ListItemIcon>
-						<ListItemText primary="Info &amp; Service" />
-					</ListItemLink>
-					<ListItemLink to="/contact">
-						<ListItemIcon><ContactIcon /></ListItemIcon>
-						<ListItemText primary="Kontakt" />
-					</ListItemLink>
-					<ListItemLink to="/mensa">
-						<ListItemIcon><CafeteriaIcon /></ListItemIcon>
-						<ListItemText primary="Mensa" />
-					</ListItemLink>
-					<ListItemLink to="/alumni">
-						<ListItemIcon><AlumniIcon /></ListItemIcon>
-						<ListItemText primary="Alumni" />
-					</ListItemLink>
-					<ListItemLink to="/planner">
-						<ListItemIcon><PlannerIcon /></ListItemIcon>
-						<ListItemText primary="Vertretungsplan" />
-					</ListItemLink>
-					<ListItemLink to="/newsletter">
-						<ListItemIcon><NewsletterIcon /></ListItemIcon>
-						<ListItemText primary="Newsletter" />
-					</ListItemLink>
-					<Box mb={3} />
-					<Divider />
-					<Box pt={3} pb={3}>
-						<EduportLaunchpad />
-					</Box>
+					{this.renderStructure(navStructure)}
 				</SideNavList>
+				<Box mb={3} />
+				<Divider />
+				<Box pt={3} pb={3}>
+					<EduportLaunchpad />
+ 				</Box>
 			</SideNav>
 		);
 	}
