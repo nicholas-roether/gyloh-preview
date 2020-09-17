a8272660a9eca35e4c961aeb3b6c7a2496c71ef4
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-17 15:55:34 +0200

    Create clip path

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -6,16 +6,18 @@ import theme from "../theme";
 export default class HomePage extends React.Component {
     render() {
         const OpeningWrapper = styled.div`
-            display: flex;
-            width: 100%;
+            display: inline-flex;
             justify-content: space-between;
             align-items: center;
+			margin: auto;
             margin-bottom: 8em;
         `;
         
         const OpeningImage = styled.img`
             width: 50%;
-            /* max-width: 600px; */
+            margin: ${theme.spacing(4)}px;
+			/* TODO make this look better */
+			clip-path: polygon(45% 0, 67% 18%, 98% 24%, 84% 54%, 91% 86%, 57% 82%, 24% 96%, 22% 65%, 0 37%, 28% 23%);
             ${theme.breakpoints.down("sm")} {
                 display: none;
             }
