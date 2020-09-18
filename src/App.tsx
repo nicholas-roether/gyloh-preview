87f6a13dc00d147b4e2ac4a1cb5a05f105835a6c
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-19 01:02:03 +0200

    Stop hiding overflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Page from "./static/Page";
 import PageRouter from "./static/Router";
+import theme from "./theme";
 
 export default class App extends React.Component {
 
 	render() {
 		return (
 			<div id="app" style={{
-				overflow: "hidden"
+				background: theme.palette.background.default
 			}}>
 				<PageRouter wrapper={Page} />
 			</div>
