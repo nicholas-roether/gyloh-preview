7998e7c27abbd15d198199db9e1b0059769c8af5
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-19 01:02:20 +0200

    Fix margins

--- src/common/Section.tsx
+++ src/common/Section.tsx
@@ -7,14 +7,14 @@ export interface SectionProps {
 }
 
 export default class Section extends React.Component<SectionProps> {
-	private readonly timeoutFactor = 100;
+	private readonly timeoutFactor = 300;
 
 	render() {
 		const { number } = this.props;
 		return (
-			<Box mb={4} pb={2}>
+			<Box mb={16}>
 				<SlideIn timeout={number ? (number - 1) * this.timeoutFactor : 0}>
-					{this.props.children}
+					<Box mb={8}>{this.props.children}</Box>
 					<Divider />
 				</SlideIn>
 			</Box>
