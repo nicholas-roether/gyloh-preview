4d4f52028254b6334ad283dee6c81b981c4356ce
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-19 23:57:15 +0200

    Adjust timeouts

--- src/common/Section.tsx
+++ src/common/Section.tsx
@@ -7,13 +7,14 @@ export interface SectionProps {
 }
 
 export default class Section extends React.Component<SectionProps> {
-	private readonly timeoutFactor = 300;
+	private readonly timeoutFactor = 100;
 
 	render() {
 		const { number } = this.props;
+		const timeout = number ? (number - 1) * this.timeoutFactor : 0;
 		return (
 			<Box mb={16}>
-				<SlideIn timeout={number ? (number - 1) * this.timeoutFactor : 0}>
+				<SlideIn timeout={timeout}>
 					<Box mb={8}>{this.props.children}</Box>
 					<Divider />
 				</SlideIn>
