acc2a7d38e023ae9e10642198d551a6b7ff0bbb1
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-20 23:17:17 +0200

    Implement NewsDisplay

--- src/home/NewsDisplay.tsx
+++ src/home/NewsDisplay.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import CardDisplay from "../common/CardDisplay";
 
-class NewsDisplay extends React.Component {
-    
+export default class NewsDisplay extends React.Component {
+    private static readonly NUM_CARDS = {
+		xs: 1,
+		sm: 2,
+		md: 3,
+		lg: 3,
+		xl: 3
+	}
+
+	render() {
+		return (
+			<CardDisplay numCards={NewsDisplay.NUM_CARDS}>
+				{this.props.children}
+			</CardDisplay>
+		);
+	}
 }
