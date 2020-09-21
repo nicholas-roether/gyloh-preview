c164962cf40a9bb2021e3308d5484b7a933223e8
Ph4ntom3 <nicholas.roether@t-online.de>
2020-09-21 18:35:29 +0200

    Adjust margins

--- src/common/Section.tsx
+++ src/common/Section.tsx
@@ -1,24 +1,42 @@
-import { Box, Divider } from "@material-ui/core";
+import { createStyles, Divider, withStyles, WithStyles } from "@material-ui/core";
 import React from "react";
+import theme from "../theme";
 import SlideIn from "./SlideIn";
+
+const styles = createStyles({
+	wrapper: {
+		marginBottom: theme.spacing(6),
+		[theme.breakpoints.up("sm")]: {
+			marginBottom: theme.spacing(10)
+		}
+	},
+	contentWrapper: {
+		marginBottom: theme.spacing(4),
+		[theme.breakpoints.up("sm")]: {
+			marginBottom: theme.spacing(8)
+		}
+	}
+})
 
 export interface SectionProps {
 	number?: number
 }
 
-export default class Section extends React.Component<SectionProps> {
+class Section extends React.Component<SectionProps & WithStyles<typeof styles>> {
 	private readonly timeoutFactor = 100;
 
 	render() {
-		const { number } = this.props;
+		const { number, classes } = this.props;
 		const timeout = number ? (number - 1) * this.timeoutFactor : 0;
 		return (
-			<Box mb={16}>
+			<div className={classes.wrapper}>
 				<SlideIn timeout={timeout}>
-					<Box mb={8}>{this.props.children}</Box>
+					<div  className={classes.contentWrapper}>{this.props.children}</div>
 					<Divider />
 				</SlideIn>
-			</Box>
+			</div>
 		);
 	}
 }
+
+export default withStyles(styles)(Section);
