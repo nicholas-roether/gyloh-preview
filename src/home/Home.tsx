25a2e623802100861b956c6e0df7cfd49157df38
nicholas-roether <nicholas.roether@t-online.de>
2020-09-23 19:52:50 +0200

    Remove SlideIns

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -2,7 +2,6 @@ import { Box, createStyles, Theme, Typography, withStyles, WithStyles } from "@m
 import React from "react";
 import DefaultButton from "../common/DefaultButton";
 import Section from "../common/Section";
-import SlideIn from "../common/SlideIn";
 import NewsCard from "./NewsCard";
 import NewsDisplay from "./NewsDisplay";
 import Star from "./Star";
@@ -33,7 +32,7 @@ class Home extends React.Component<WithStyles<typeof styles>> {
 
         return (
             <div>
-				<Section number={1}>
+				<Section>
 					<div className={classes.openingWrapper}>
 						<span className={classes.textWrapper}>
 							<Typography variant="h3" gutterBottom>Herzlich Willkommen</Typography>
@@ -41,38 +40,34 @@ class Home extends React.Component<WithStyles<typeof styles>> {
 								fontStyle: "italic",
 								fontWeight: "lighter"
 							}}>"Mit dem Wir zum Ich - fördere dein Wissen und erweitere deinen Horizont!"</Typography>
-							<SlideIn from="right">
-								<Box mt={3} whiteSpace="nowrap">
-									<DefaultButton to="/about">Über Uns</DefaultButton>
-									<DefaultButton>Vertretungsplan</DefaultButton>
-								</Box>
-							</SlideIn>
+							<Box mt={3} whiteSpace="nowrap">
+								<DefaultButton to="/about">Über Uns</DefaultButton>
+								<DefaultButton>Vertretungsplan</DefaultButton>
+							</Box>
 						</span>
 						<Star />
 					</div>
 				</Section>
-				<Section number={2}>
+				<Section>
                 	<Typography variant="h4" gutterBottom={true}>News</Typography>
-					<SlideIn from="left">
-						<NewsDisplay>
-							<NewsCard heading="Schülerin verreckt elendig in Minecraft" more="#">
-								Eine Schülerin der Klasse 7e wurde gestern gegen 20:21 Uhr absolut krank von einem weiteren Schüler in Minecraft Skywars weggerekt.
-							</NewsCard>
-							<NewsCard heading="Aufnahmeritual der Fünftklässler" more="#">
-								Am 17.09 fand endlich die traditionelle Opfergabenzeremonie der neuen fünften Klassen statt. Die Kinder schlugen sich alle
-								ziemlich gut in dem zeremoniellen blutigen Todeskampf, Zehn wurden schwer verletzt und es gab sogar drei Tote! Schlussendlich
-								fand die Jungfrauopferung statt - Man konnte die Schreie von Lena aus der 5b viele Kilometer weit hören!
-							</NewsCard>
-							<NewsCard heading="日本語書ければどうなるの" more="#">
-								これはただのテストだ。このサイトで日本語はどう見えるのが知りたい。
-							</NewsCard>
-							<NewsCard heading="pingeon exxist t ??/?" more="#">
-								Wap wap wappu wap
-							</NewsCard>
-						</NewsDisplay>
-					</SlideIn>
+					<NewsDisplay>
+						<NewsCard heading="Schülerin verreckt elendig in Minecraft" more="#">
+							Eine Schülerin der Klasse 7e wurde gestern gegen 20:21 Uhr absolut krank von einem weiteren Schüler in Minecraft Skywars weggerekt.
+						</NewsCard>
+						<NewsCard heading="Aufnahmeritual der Fünftklässler" more="#">
+							Am 17.09 fand endlich die traditionelle Opfergabenzeremonie der neuen fünften Klassen statt. Die Kinder schlugen sich alle
+							ziemlich gut in dem zeremoniellen blutigen Todeskampf, Zehn wurden schwer verletzt und es gab sogar drei Tote! Schlussendlich
+							fand die Jungfrauopferung statt - Man konnte die Schreie von Lena aus der 5b viele Kilometer weit hören!
+						</NewsCard>
+						<NewsCard heading="日本語書ければどうなるの" more="#">
+							これはただのテストだ。このサイトで日本語はどう見えるのが知りたい。
+						</NewsCard>
+						<NewsCard heading="pingeon exxist t ??/?" more="#">
+							Wap wap wappu wap
+						</NewsCard>
+					</NewsDisplay>
 				</Section>
-				<Section number={3}>
+				<Section>
 					<Box textAlign="center">
 						{/* Put an image here */}
 						<Typography variant="h4">Bleiben Sie auf dem neuesten Stand.</Typography>
