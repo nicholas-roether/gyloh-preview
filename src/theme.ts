93d7f45484a1ba7aca6b9e36cce3e20217b82eda
nicholas-roether <nicholas.roether@t-online.de>
2020-09-23 14:06:39 +0200

    Implement new themes

--- src/theme.ts
+++ src/theme.ts
@@ -1,47 +1,64 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme, SimplePaletteColorOptions } from "@material-ui/core/styles";
+import { TypographyOptions } from "@material-ui/core/styles/createTypography";
 
-const theme = createMuiTheme({
+const typography: TypographyOptions = {
+	fontFamily: "Roboto, sans-serif",
+	fontSize: 13,
+	button: {
+		fontSize: 15,
+		fontFamily: "Rubik, sans-serif",
+		textTransform: "uppercase",
+	},
+	h1: {
+		fontFamily: "Rubik, sans-serif"
+	},
+	h2: {
+		fontFamily: "Rubik, sans-serif"
+	},
+	h3: {
+		fontFamily: "Rubik, sans-serif"
+	},
+	h4: {
+		fontFamily: "Rubik, sans-serif"
+	},
+	h5: {
+		fontFamily: "Rubik, sans-serif"
+	},
+	h6: {
+		fontFamily: "Rubik, sans-serif"
+	},
+}
+
+const primary: SimplePaletteColorOptions = {
+	main: "#002860",
+	contrastText: "#fff",
+}
+
+const secondary: SimplePaletteColorOptions = {
+	main: "#6184d8",
+	contrastText: "#fff",
+}
+
+export const lightTheme = createMuiTheme({
 	palette: {
-		primary: {
-			main: "#002860",
-			contrastText: "#fff",
-		},
-		secondary: {
-			main: "#6184d8",
-			contrastText: "#fff",
-			
-		},
+		type: "light",
+		primary,
+		secondary,
 		background: {
 			default: "#fff"
 		}
 	},
-	typography: {
-		fontFamily: "Roboto, sans-serif",
-		fontSize: 13,
-		button: {
-			fontSize: 15,
-			fontFamily: "Rubik, sans-serif",
-			textTransform: "uppercase",
-		},
-		h1: {
-			fontFamily: "Rubik, sans-serif"
-		},
-		h2: {
-			fontFamily: "Rubik, sans-serif"
-		},
-		h3: {
-			fontFamily: "Rubik, sans-serif"
-		},
-		h4: {
-			fontFamily: "Rubik, sans-serif"
-		},
-		h5: {
-			fontFamily: "Rubik, sans-serif"
-		},
-		h6: {
-			fontFamily: "Rubik, sans-serif"
+	typography
+});
+
+export const darkTheme = createMuiTheme({
+	palette: {
+		type: "dark",
+		primary,
+		secondary,
+		background: {
+			default: "#131313",
+			paper: "#373f43"
 		},
 	},
 });
-
-export default theme;
