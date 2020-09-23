53272fed4454b0bd46b6c34a1d6d3b3eaa0fb246
nicholas-roether <nicholas.roether@t-online.de>
2020-09-23 14:06:18 +0200

    Make themes part of app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import { WithTheme, withTheme } from "@material-ui/core";
+import { Box, Theme, ThemeProvider } from "@material-ui/core";
 import React from "react";
 import Page from "./static/Page";
 import PageRouter from "./static/Router";
 
-class App extends React.Component<WithTheme> {
+interface AppState<N> {
+	theme: N
+}
+
+export type ThemeMap<N extends string> = Record<N, Theme>;
+
+export interface AppProps<T extends ThemeMap<N>, N extends string> {
+	initialTheme: N,
+	themes: T
+}
+
+export default class App<T extends ThemeMap<N>, N extends string> extends React.Component<AppProps<T, N>, AppState<N>> {
+	state = {theme: this.props.initialTheme}
+
 	render() {
-		const { theme } = this.props;
+		const theme = this.props.themes[this.state.theme] as Theme;
 		return (
-			<div id="app" style={{
-				background: theme.palette.background.default
-			}}>
-				<PageRouter wrapper={Page} />
-			</div>
+			<ThemeProvider theme={theme}>
+				<Box id="app" bgcolor={theme.palette.background.default} height="100%" color={theme.palette.type === "light" ? "#000" : "#fff"}>
+					<PageRouter wrapper={Page} />
+				</Box>
+			</ThemeProvider>
 		);
 	}
 }
-
-export default withTheme(App);
