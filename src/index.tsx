5c43b48d10be3bc2d9601f8b6e2c9bf3862893d4
nicholas-roether <nicholas.roether@t-online.de>
2020-09-23 14:06:31 +0200

    Adjust for new App implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import { ThemeProvider } from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import theme from "./theme";
+import { lightTheme, darkTheme} from "./theme";
 import { adjustHref } from "./util";
 
 adjustHref();
 
+const themeMap = {
+	"light": lightTheme,
+	"dark": darkTheme
+}
+
 ReactDOM.render(
-	<ThemeProvider theme={theme}>
-		<App />
-	</ThemeProvider>, 
+	<App themes={themeMap} initialTheme="light" />,
 	document.getElementById("root")
 );
