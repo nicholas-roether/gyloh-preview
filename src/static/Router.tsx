910414a36358560068c6d7e3f4bd6da61e1c4762
nicholas-roether <nicholas.roether@t-online.de>
2020-09-23 19:52:59 +0200

    Add Fade

--- src/static/Router.tsx
+++ src/static/Router.tsx
@@ -1,3 +1,4 @@
+import { Fade } from "@material-ui/core";
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Error404 from "../error/Error404";
@@ -12,14 +13,16 @@ export default class PageRouter extends React.Component<RouterProps> {
 	render() {
 		return (
 			<Router basename={process.env.PUBLIC_URL}>
-				<Page onThemeChange={this.props.onThemeChage}>
-					<Switch>
-						<Route path="/" exact>
-							<Home />
-						</Route>
-						<Route path="*" component={Error404} />
-					</Switch>
-				</Page>
+				<Fade in={true} appear={true} timeout={0}>
+					<Page onThemeChange={this.props.onThemeChage}>
+						<Switch>
+							<Route path="/" exact>
+								<Home />
+							</Route>
+							<Route path="*" component={Error404} />
+						</Switch>
+					</Page>
+				</Fade>
 			</Router>
 		);
 	}
