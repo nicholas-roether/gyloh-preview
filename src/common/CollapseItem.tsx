a27a953c7434b934eff79c39a39e8de57af89edb
nicholas-roether <nicholas.roether@t-online.de>
2020-09-23 19:34:59 +0200

    Change API around a bit

--- src/common/CollapseItem.tsx
+++ src/common/CollapseItem.tsx
@@ -1,20 +1,26 @@
-import { createStyles, Theme, withStyles, WithStyles } from "@material-ui/core";
+import { createStyles, ListItemIcon, ListItemText, Theme, withStyles, WithStyles, withTheme, WithTheme } from "@material-ui/core";
+import { SubdirectoryArrowLeft, SubdirectoryArrowRight } from "@material-ui/icons";
 import React from "react";
 import LinkNavOption, { LinkNavOptionProps } from "./LinkNavOption";
 
 const styles = (theme: Theme) => createStyles({
 	nested: {
-		paddingLeft: theme.spacing(10)
+		paddingLeft: theme.spacing(3)
 	}
 });
 
 export type CollapseItemProps = Omit<LinkNavOptionProps, "classes">;
 
-class CollapseItem extends React.Component<CollapseItemProps & WithStyles<typeof styles>> {
+class CollapseItem extends React.Component<CollapseItemProps & WithStyles<typeof styles> & WithTheme> {
 	render() {
-		const { classes, ...other } = this.props;
-		return <LinkNavOption classes={{button: classes.nested}} {...other}></LinkNavOption>
+		const { classes, children, theme, ...other } = this.props;
+		return (
+			<LinkNavOption classes={{button: classes.nested}} {...other}>
+				<ListItemIcon>{theme.direction === "ltr" ? <SubdirectoryArrowRight /> : <SubdirectoryArrowLeft />}</ListItemIcon>
+				<ListItemText primary={children}/>
+			</LinkNavOption>
+		);
 	}
 }
 
-export default withStyles(styles)(CollapseItem);
+export default withStyles(styles)(withTheme(CollapseItem));
