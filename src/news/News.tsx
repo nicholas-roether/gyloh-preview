9a9e9fd2d7b97081eb7d0fdc4ed130ef985c786e
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-03 18:25:37 +0200

    Implement News page

--- src/news/News.tsx
+++ src/news/News.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import MuiMD from "../common/MuiMD";
+import Error404 from "../error/Error404";
+import NewsModel from "../model/NewsModel";
 
-class News extends React.Component<WithRouter> {
-	render() {
-		
+function News(): React.ReactElement {
+	const [content, setContent] = useState<string | null>(null);
+	const { name } = useParams<{name: string}>();
+	if(!name) return <Error404 location={window.location} />;
+	if(!content) {
+		NewsModel.getNews(name).then(model => setContent(model.markdown)).catch(() => setContent("news not found"));
+		return <div>Loading...</div>; // TODO create loading indicator
 	}
+	return <MuiMD>{content}</MuiMD>
 }
 
 export default News;
