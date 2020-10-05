f8ef3b60b4ed4c6256994e486b3b30c169af8a2b
nicholas-roether <nicholas.roether@t-online.de>
2020-10-05 19:34:49 +0200

    Move page from router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { Box, makeStyles, Theme, ThemeProvider } from "@material-ui/core";
 import React from "react";
+import Page from "./static/Page";
 import PageRouter from "./static/PageRouter";
 
 type ThemeMap = Record<string, Theme>;
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
 	app: {
 		overflow: "hidden",
 		height: "100%",
+	},
+	page: {
+		transition: theme.transitions.create(["transform, opacity"], {
+			easing: theme.transitions.easing.easeIn,
+			duration: "200ms"
+		})
 	}
-});
+}));
 
 export interface AppProps {
 	initialTheme: string,
@@ -20,6 +27,7 @@ const App: React.FC<AppProps> = props => {
 	const [themeName, setThemeName] = React.useState<string>(props.initialTheme);
 	const classes = useStyles();
 	const theme = props.themes[themeName];
+	const onThemeChange = () => setThemeName(prev => prev === "light" ? "dark" : "light");
 	return (
 		<ThemeProvider theme={theme}>
 			<Box 
@@ -28,7 +36,13 @@ const App: React.FC<AppProps> = props => {
 				bgcolor={theme.palette.background.default}
 				color={theme.palette.text.primary}
 			>
-				<PageRouter onThemeChage={() => setThemeName(prev => prev === "light" ? "dark" : "light")} />
+				<PageRouter>
+					{content => (
+						<Page onThemeChange={onThemeChange}>
+							{content}
+						</Page>
+					)}
+				</PageRouter>
 			</Box>
 		</ThemeProvider>
 	)
--- src/static/PageRouter.tsx
+++ src/static/PageRouter.tsx
@@ -1,43 +1,35 @@
-import { makeStyles, Theme } from "@material-ui/core";
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import ExternalRedirect from "../common/ExternalRedirect";
 import Error404 from "../error/Error404";
 import Home from "../home/Home";
 import News from "../news/News";
-import Page from "./Page";
 
-const useStyles = makeStyles((theme: Theme) => ({
-	page: {
-		transition: theme.transitions.create(["transform, opacity"], {
-			easing: theme.transitions.easing.easeIn,
-			duration: "200ms"
-		})
-	}
-}));
+export type RouteHander = (content: React.ReactElement) => React.ReactElement;
 
 export interface PageRouterProps {
-	onThemeChage?: () => void;
+	children: RouteHander
 }
 
 const PageRouter: React.FC<PageRouterProps> = props => {
-	const classes = useStyles();
+	const handler = props.children;
+	const switchElem = (
+		<Switch>
+			<Route path="/" exact>
+				<Home />
+			</Route>
+			<Route path="/news/:name">
+				<News />
+			</Route>
+			<Route path="/newsletter">
+				<ExternalRedirect href="https://subscribe.newsletter2go.com/?n2g=wi5y2fvr-plkndw13-j07&_ga=2.85204825.1133588827.1587851132-316422097.1587286407" />
+			</Route>
+			<Route path="*" component={Error404} />
+		</Switch>
+	)
 	return (
 		<Router basename={process.env.PUBLIC_URL}>
-			<Page onThemeChange={props.onThemeChage} className={classes.page}>
-				<Switch>
-					<Route path="/" exact>
-						<Home />
-					</Route>
-					<Route path="/news/:name">
-						<News />
-					</Route>
-					<Route path="/newsletter">
-						<ExternalRedirect href="https://subscribe.newsletter2go.com/?n2g=wi5y2fvr-plkndw13-j07&_ga=2.85204825.1133588827.1587851132-316422097.1587286407" />
-					</Route>
-					<Route path="*" component={Error404} />
-				</Switch>
-			</Page>
+			{handler(switchElem)}
 		</Router>
 	);
 }
