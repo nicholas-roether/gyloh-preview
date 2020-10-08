18063d09bea0b19ffa9ce054c3023c35be04af7e
nicholas-roether <nicholas.roether@t-online.de>
2020-10-08 02:24:35 +0200

    Add customizability to default button

--- src/common/DefaultButton.tsx
+++ src/common/DefaultButton.tsx
@@ -1,32 +1,29 @@
-import { Box, Button } from "@material-ui/core";
+import { Box, Button, ButtonProps } from "@material-ui/core";
 import React from "react";
 import { Link } from "react-router-dom";
 
-export interface DefaultButtonProps {
-	onClick?: React.MouseEventHandler,
-	variant?: "text" | "outlined" | "contained",
-	color?: "inherit" | "primary" | "secondary" | "default",
-	href?: string,
-	margin?: "right" | "left" | "both" | "none" | null,
+export interface DefaultButtonProps extends ButtonProps {
+	margin?: "right" | "left" | "both" | "none",
 	to?: string;
 }
 
 const DefaultButton: React.FC<DefaultButtonProps> = props => {
+	const { margin, to, children, ...other } = props;
 	const button = (
-		<Button variant={props.variant || "contained"} color={props.color || "secondary"} onClick={props.onClick} href={props.href}>
-			{props.children}
+		<Button variant="contained" color="secondary" {...other}>
+			{children}
 		</Button>
 	);
 	return (
 		<Box 
-			mr={!props.margin || ["right", "both"].includes(props.margin) ? 2 : 0} 
-			ml={props.margin && ["left", "both"].includes(props.margin) ? 2 : 0}
+			mr={!margin || ["right", "both"].includes(margin) ? 2 : 0} 
+			ml={margin && ["left", "both"].includes(margin) ? 2 : 0}
 			marginY={1}
 			display="inline-block"
 		>
 			{
-				props.to ? 
-				<Link to={props.to} style={{textDecoration: "none"}}>
+				to ? 
+				<Link to={to} style={{textDecoration: "none"}}>
 					{button}
 				</Link>
 				:
