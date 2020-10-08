a5833f0b718f69d47e15076ddcb28e6bd267d94d
nicholas-roether <nicholas.roether@t-online.de>
2020-10-08 02:28:42 +0200

    Add text util comps and new Sections

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,8 +1,12 @@
 import { Box, makeStyles, Theme, Typography } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
 import DefaultButton from "../common/DefaultButton";
+import Heading from "../common/Heading";
+import Paragraph from "../common/Paragraph";
 import Section from "../common/Section";
 import NewsDisplay from "./NewsDisplay";
+import PartnerDisplay from "./PartnerDisplay";
 import Star from "./Star";
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -32,7 +36,7 @@ const Home: React.FC = props => {
 			<Section>
 				<div className={classes.openingWrapper}>
 					<span className={classes.textWrapper}>
-						<Typography variant="h3" gutterBottom>Herzlich Willkommen</Typography>
+						<Heading variant="h3" gutterBottom>Herzlich Willkommen</Heading>
 						<Typography variant="subtitle1" style={{
 							fontStyle: "italic",
 							fontWeight: "lighter"
@@ -45,15 +49,30 @@ const Home: React.FC = props => {
 					<Star />
 				</div>
 			</Section>
+			<Section align="left">
+				<Heading variant="h4">Covid-19</Heading>
+				<Paragraph>
+					Das Gymnasium Lohbrügge ergreift Maßnahmen für die Sicherheit unserer Schüler und unserer Angestellten, und die Eindämmung der Covid-19 Epidemie.
+				</Paragraph>
+				<Paragraph>
+					Aktuelle Informationen finden sie <Link to="/news/covid-19">hier</Link>.
+				</Paragraph>
+			</Section>
 			<Section>
-				<Typography variant="h4" gutterBottom={true}>News</Typography>
+				<Heading variant="h4">News</Heading>
 				<NewsDisplay />
 			</Section>
 			<Section>
 				<Box textAlign="center">
 					{/* Put an image here */}
-					<Typography variant="h4">Bleiben Sie auf dem neuesten Stand.</Typography>
+					<Heading variant="h4">Bleiben Sie auf dem neuesten Stand.</Heading>
 					<DefaultButton to="/newsletter">Newsletter beitreten</DefaultButton>
+				</Box>
+			</Section>
+			<Section>
+				<Heading variant="h4">Partner und Auszeichnungen</Heading>
+				<Box mt={2}>
+					<PartnerDisplay />
 				</Box>
 			</Section>
 		</div>
