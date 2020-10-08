df66c83955007dd17e7082797e3e78b343a75e96
nicholas-roether <nicholas.roether@t-online.de>
2020-10-08 02:28:17 +0200

    Allow custom text alignment and add margin

--- src/common/Section.tsx
+++ src/common/Section.tsx
@@ -1,11 +1,24 @@
 import { Divider, makeStyles, Theme } from "@material-ui/core";
 import React from "react";
+import { classesIf } from "../util";
 
 const useStyles = makeStyles((theme: Theme) => ({
 	wrapper: {
 		paddingBottom: theme.spacing(6),
 		[theme.breakpoints.up("sm")]: {
 			paddingBottom: theme.spacing(10)
+		}
+	},
+	wrapperLeft: {
+		textAlign: "left",
+		[theme.breakpoints.up("sm")]: {
+			marginRight: "20%"
+		}
+	},
+	wrapperRight: {
+		textAlign: "right",
+		[theme.breakpoints.up("sm")]: {
+			marginLeft: "20%"
 		}
 	},
 	contentWrapper: {
@@ -16,11 +29,22 @@ const useStyles = makeStyles((theme: Theme) => ({
 	}
 }));
 
-const Section: React.FC = props => {
+export interface SectionProps {
+	align?: "left" | "right" | "full"
+}
+
+const Section: React.FC<SectionProps> = props => {
 	const classes = useStyles();
+	const align = props.align || "full";
 	return (
 		<div className={classes.wrapper}>
-			<div  className={classes.contentWrapper}>{props.children}</div>
+			<div  className={classesIf(
+				classes.contentWrapper,
+				[classes.wrapperLeft, align === "left"],
+				[classes.wrapperRight, align === "right"]
+			)}>
+				{props.children}
+			</div>
 			<Divider />
 		</div>
 	);
